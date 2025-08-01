e8e4c66884efa89d918df1a0232d71cf16ee9888
Ignacio Molina <molinagranda@gmail.com>
2025-08-01 15:26:25 -0300

    Commit Inicial

---
+++ src/test/java/com/mercadopago/mpsdk_example/MpsdkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadopago.mpsdk_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpsdkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
